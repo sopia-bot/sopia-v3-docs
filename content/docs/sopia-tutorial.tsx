a4cca57f719d82e1b9f04e96c878691fae07ae5b
raravel <youn@tree-some.dev>
2025-12-05 00:30:04 +0900

    튜토리얼 추가

---
+++ content/docs/sopia-tutorial.tsx
@@ -0,0 +1,15 @@
+export function ArcadeEmbed() {
+  return (
+    <div style={{ position: 'relative', paddingBottom: 'calc(69.89247311827957% + 41px)', height: '0', width: '100%' }}>
+      <iframe
+        src="https://demo.arcade.software/0XHtgHMN0GWJBrAjIDUy?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="소피아 튜토리얼"
+        frameBorder="0"
+        loading="lazy"
+        allowFullScreen
+        allow="clipboard-write"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', colorScheme: 'light' }}
+      />
+    </div>
+  )
+}
