99c9e4b69e136b56e101463f653e38ec29cc41be
raravel <youn@tree-some.dev>
2025-12-06 16:52:50 +0900

    스푼 캡처 튜토리얼

---
+++ content/docs/spoon-taxi/taxi-tutorial.tsx
@@ -0,0 +1,15 @@
+export function ArcadeEmbed() {
+  return (
+    <div style={{ position: 'relative', paddingBottom: 'calc(54.37499999999999% + 41px)', height: '0', width: '100%' }}>
+      <iframe
+        src="https://demo.arcade.software/hthf8U6jsPNqAbndXryL?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="스푼 캡처 기능"
+        frameBorder="0"
+        loading="lazy"
+        allowFullScreen
+        allow="clipboard-write"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', colorScheme: 'light' }}
+      />
+    </div>
+  )
+}
